9a7b0f95bcd24890f30bdf0ab8b9308c495e3d1f
Ronald Oussoren <ronaldoussoren@mac.com>
2011-07-05 22:10:08 +0200

    Record source locations, also handle variable definitions (incompletely)

--- objective/metadata/parsing.py
+++ objective/metadata/parsing.py
@@ -14,8 +14,17 @@ class HeaderParser (object):
 
         self._headers = set()
         self._definitions = {
-            'enum': set(),
+            'enum': [],
+            'variable': [],
+            'struct': [],
+            'typedef': [],
+            'function': {},
+            'interface': {},
+            'protocol': {},
+            'informal_protocol': {},
         }
+        self._files = {}
+
 
     def parse(self):
         unit = self._index.parse("meta-extract.m",
@@ -52,6 +61,15 @@ class HeaderParser (object):
             return True
 
         return False
+
+    def location_info(self, location):
+        fn = location.file.name
+        if fn in self._files:
+            fn =  self._files[fn]
+        else:
+            fn = self._files[fn] = _remap(self._framework_chk, fn)
+
+        return { 'file': fn, 'line': location.line, 'column': location.column }
 
     _nodemap = {}
 
@@ -92,7 +110,10 @@ class HeaderParser (object):
     def _handle_enum_decl(self, node):
         for label in node.get_children():
             assert label.kind is cindex.CursorKind.ENUM_CONSTANT_DECL, label.kind
-            self._definitions['enum'].add(label.spelling)
+            self._definitions['enum'].append({
+                'name': label.spelling,
+                'location': self.location_info(label.location),
+            })
 
     @register(cindex.CursorKind.OBJC_PROTOCOL_DECL)
     def _handle_protocol_decl(self, node):
@@ -106,10 +127,34 @@ class HeaderParser (object):
     def _handle_category_decl(self, node):
         pass
 
+    @register(cindex.CursorKind.VAR_DECL)
+    def _var_decl(self, node):
+        #print node.spelling
+        #print node.location.file.name, node.location.line, node.location.column
+
+        # FIXME: For some reason we don't always get a type here
+        try:
+           type = node.get_children().next().displayName
+        except StopIteration:
+            type = None
+
+        self._definitions['variable'].append({
+            'name': node.spelling,
+            'location': self.location_info(node.location),
+            'type': type
+        })
+
+
 
     del register
 
+def _remap(frm_chk, path):
+    i = path.find(frm_chk)
+    v = path[i+8+len(frm_chk):]
+    return v
 
 if __name__ == "__main__":
     p = HeaderParser("Foundation")
     p.parse()
+    import pprint
+    pprint.pprint (p._definitions)
